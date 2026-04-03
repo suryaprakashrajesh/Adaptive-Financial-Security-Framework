a585017f8e19fcf181444e4812d0fea4ca9fd3be
Surya Prakash R <147045906+suryaprakashrajesh@users.noreply.github.com>
2026-04-03 14:13:09 +0530

    Fix indentation for app.run in app.py

--- app.py
+++ app.py
@@ -925,5 +925,7 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
     # Create SSL context
-    if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
+
+    
